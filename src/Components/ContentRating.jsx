8c83d76a4c2946d5aeffbc9a6ab23035480b38fe
aubin-pierre7 <aubinp58@gmail.com>
2026-01-05 12:33:16 +0100

    Quelques modifications

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -1,18 +1,45 @@
+ import React, { Component } from 'react';
+ import './ContentRating.css';
 
-import React, { Component } from 'react';
-import './ContentRating.css';
-
-class ContentRating extends Component {
-  constructor() {
-    super();
-  }
-  render() {
-    return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
-    );
-  }
-}
-
-export default ContentRating;
+ class ContentRating extends Component {
+   constructor() {
+     super();
+     this.state = {
+       likes: 0,
+       dislikes: 0,
+       totalRatings: 0,
+       handleLike: () => {
+         this.setState((prevState) => ({
+           likes: prevState.likes + 1,
+           totalRatings: prevState.totalRatings + 1
+         }));
+       },
+       handleDislike: () => {
+         this.setState((prevState) => ({
+           dislikes: prevState.dislikes + 1,
+           totalRatings: prevState.totalRatings + 1
+         }));
+       }
+     };
+   }
+   render() {
+     return (
+       <>
+         <h1>Évaluation du Contenu Textuel</h1>
+         <div className='content-rating'>
+           <p>Texte</p>
+           <div className='rating-buttons'>
+             <button className="like-button" onClick={this.state.handleLike}>
+               J'aime ({this.state.likes})
+             </button>
+             <button className="dislike-button" onClick={this.state.handleDislike}>
+               Je n'aime pas ({this.state.dislikes})
+             </button>
+           </div>
+           <p>Total des Évaluations : {this.state.totalRatings}</p>
+         </div>
+       </>
+     );
+   }
+ }
+ export default ContentRating;
